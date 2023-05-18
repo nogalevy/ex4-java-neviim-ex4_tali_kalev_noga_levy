42403d4c20dadbdc4604ed4153628def4b2a971b
Tali Kalev <talkalev@gmail.com>
2023-05-18 17:14:22 +0300

    removed search button and added search icon

--- movie-app/src/components/SearchInput.js
+++ movie-app/src/components/SearchInput.js
@@ -5,25 +5,24 @@ import useFetch from "./useFetch";
 
 const TRENDING_PAGE = '/trending/all/week?&language=en-US';
 // const TRENDING_PAGE ='/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=99'
-export default function SearchInput({inputType, clearGenres}){
+export default function SearchInput({inputType, clearGenres}) {
     const [url, setUrl] = useState(TRENDING_PAGE)
     const [submitInput, setSubmitInput] = useState('');
-    const { error, isPending, data } = useFetch(url);
+    const {error, isPending, data} = useFetch(url);
     const {setMoviesData} = useMoviesContext()
 
     //validate by input
 
-    useEffect(()=>{
+    useEffect(() => {
         setMoviesData({error, isPending, data})
-    },[error, isPending, data])
+    }, [error, isPending, data])
 
     const handleSubmit = (e) => {
         e.preventDefault();
         let u;
-        if (inputType === InputTypes.TITLE){
+        if (inputType === InputTypes.TITLE) {
             u = `/search/multi?query=${submitInput}&include_adult=false&language=en-US&page=1`
-        }
-        else if(inputType === InputTypes.YEAR){
+        } else if (inputType === InputTypes.YEAR) {
             u = `/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=${submitInput}&sort_by=primary_release_date.desc`
         }
         setUrl(u);
@@ -31,12 +30,12 @@ export default function SearchInput({inputType, clearGenres}){
         clearGenres()
     }
 
-    return(
-        <form className="d-flex" role="search" onSubmit={handleSubmit}>
-            <input className="form-control me-2" type="search" placeholder={`Search by ${inputType}`} aria-label="Search"
+    return (
+        <form className="d-flex input-box" role="search" onSubmit={handleSubmit}>
+            <input className="form-control" type="search" placeholder={`Search by ${inputType}`}
+                   aria-label="Search"
                    required value={submitInput} onChange={(e) => setSubmitInput(e.target.value)}/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <i className="fa fa-search"></i>
         </form>
-
     );
 }
