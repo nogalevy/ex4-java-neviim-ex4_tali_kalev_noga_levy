e34be68333aae3356878035d3527b442ae64b5bc
Tali Kalev <talkalev@gmail.com>
2023-05-22 14:06:14 +0300

    added dropdown to search, clicking on history item is identified

--- movie-app/src/components/SearchInput.js
+++ movie-app/src/components/SearchInput.js
@@ -50,42 +50,81 @@ export default function SearchInput({inputType, clearGenres}) {
     }
 
     const renderDropdown = () => {
-        return(
-            <div className="dropdown-menu-dark absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-56 overflow-y-auto">
-                {historyState.history.map((item) => {
-                return (
+        // return(
+        //     // <div className="dropdown-menu-dark absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-56 overflow-y-auto">
+        //     // <div className={`dropdown-menu ${showDropdown ? "show" : ""}`} >
+        //         {historyState.history.map((item) => {
+        //         return (
+        //         <li>
+            //         <a className="dropdown-item" href="#">
+            //            <p onClick={(e)=>{console.log(e.target)}}>{item}</p>
+            //         </a>
+        //         </li>
+        //         )
+        //     })}
+
+        return historyState.history.map((item, index) => {
+            return (
                 <li>
-                <a className="dropdown-item" href="#">
-                <p>{item}</p>
-                </a>
+                    <a className="dropdown-item" onClick={(e)=>{setSubmitInput(item); handleURL()}} href="#">
+                        <div className="form-check">
+                            <input className="form-check-input"
+                                   onChange={(e)=>{console.log(e.target)}}
+                                   type="checkbox"
+                                   checked={false} value="" id={item}/>
+                            <label className="form-check-label" htmlFor={item}>{item}</label>
+                        </div>
+                    </a>
                 </li>
-                )
-            })}
-            </div>
-        )
+            )
+        })
+            // </div>
+
     }
 
     return (
-        <div className="container-fluid">
-        {/*<div className="dropdown">*/}
-        {/*    <a className="dropdown-toggle" href="\" role="button" data-bs-toggle="dropdown"*/}
-        {/*       aria-expanded="false">*/}
-        {/*    </a>*/}
-        <form className="d-flex input-box form-floating mb-3" role="search" onSubmit={handleSubmit}>
-            <input className="form-control" type="search" placeholder={`Search by ${inputType}`}
-                   aria-label="Search"
-                   id="searchBar"
-                   required value={submitInput} onChange={(e) => setSubmitInput(e.target.value)}
-                    onClick={handleClick}/>
-            <label htmlFor="searchBar">{`Search by ${inputType}`}</label>
-            {/*TODO: NOGA: TALI: i dont kknow how to say it in inglesh*/}
-            <i className="fa fa-search"></i>
-        </form>
-            {/*{showDropdown && renderDropdown()}*/}
+        <form  className="nav-item dropdown" role="search" onSubmit={handleSubmit}>
+        {/*<li className="nav-item dropdown">*/}
+            <input type="text" className="text-dark nav-link dropdown-toggle"
+                   // role="button"
+                   data-bs-toggle="dropdown"
+                   aria-expanded="false"
+                   value={submitInput} onChange={(e) => setSubmitInput(e.target.value)}
+            >
+            </input>
             <ul className="dropdown-menu">
                 {renderDropdown()}
             </ul>
-        {/*</div>*/}
-        </div>
+        </form>
+        // <div className="container-fluid" >
+        // {/*<div className="dropdown">*/}
+        // {/*    <a className="dropdown-toggle" href="\" role="button" data-bs-toggle="dropdown"*/}
+        // {/*       aria-expanded="false">*/}
+        // {/*    </a>*/}
+        // <form className="d-flex input-box form-floating" role="search" onSubmit={handleSubmit}>
+        //     <input className="form-control dropdown-toggle" type="search" placeholder={`Search by ${inputType}`}
+        //            aria-label="Search"
+        //            id="searchBar"
+        //            autoComplete="off"
+        //            required value={submitInput} onChange={(e) => setSubmitInput(e.target.value)}
+        //             onClick={handleClick}
+        //            onBlur={(e)=> {console.log(e.target); /*setShowDropdown(false)*/}}
+        //     // data-bs-toggle="dropdown"
+        //     //        aria-expanded="false"
+        //                />
+        //     <label htmlFor="searchBar">{`Search by ${inputType}`}</label>
+        //     {/*TODO: NOGA: TALI: i dont kknow how to say it in inglesh*/}
+        //     <i className="fa fa-search"></i>
+        // </form>
+        //     <div >
+        //
+        //     {showDropdown && renderDropdown()}
+        //     </div>
+        //     {/*<ul className="dropdown-menu">*/}
+        //     {/*    {renderDropdown()}*/}
+        //     {/*</ul>*/}
+        // {/*</div>*/}
+        // </div>
+
     );
 }
