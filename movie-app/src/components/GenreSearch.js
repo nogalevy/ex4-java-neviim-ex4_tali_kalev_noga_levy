83b8c83d524a7b1576acabac9ba76cee617cbfcd
Tali Kalev <talkalev@gmail.com>
2023-05-27 14:16:31 +0300

    fixed last unchecked not registering genre bug

--- movie-app/src/components/GenreSearch.js
+++ movie-app/src/components/GenreSearch.js
@@ -1,15 +1,14 @@
 import useFetch from "./useFetch";
 import {useEffect} from "react";
-import {GENRE_LIST, Action} from "../consts/consts";
+import {GENRE_LIST, Action, TRENDING_PAGE} from "../consts/consts";
 
 export default function GenreSearch({genreState, genreDispatch, setUrl}){
     //AND between genres, %2C, OR between genres %7C
     const { data:genreData } = useFetch(GENRE_LIST);
 
     useEffect(() => {
-        console.log('Selected genres:', genreState.list);
+        console.log("in use effect")
         if (genreState.list.length > 0){
-            //tali: this makes it so that if there is one left checked and you uncheck it it does nothing
             let genres = genreState.list.join(',')
             let u = `/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genres}`
             setUrl(u)
@@ -19,11 +18,13 @@ export default function GenreSearch({genreState, genreDispatch, setUrl}){
     const handleChange = (event) => {
         const genreId = event.currentTarget.id;
         if (event.target.checked) {
-            console.log('Adding genre:', genreId);
             genreDispatch({ type: Action.ADD, payload: genreId });
         } else {
-            console.log('Removing genre:', genreId);
             genreDispatch({ type: Action.DELETE, payload: genreId });
+            //when one genre left is unchecked, return to trending page - checks === 1 since dispatch is async
+            if(genreState.list.length === 1){
+                setUrl(TRENDING_PAGE)
+            }
         }
     };
 
