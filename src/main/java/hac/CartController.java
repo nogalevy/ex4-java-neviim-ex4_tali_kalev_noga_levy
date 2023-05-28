318fbf6c6533e39316d522960c0bf4da10f0a736
nogalevy <nogalevy2@gmail.com>
2023-05-28 17:54:10 +0300

    return errror 400 if try to add item to cart that already exist

--- src/main/java/hac/CartController.java
+++ src/main/java/hac/CartController.java
@@ -4,6 +4,7 @@ import hac.beans.CartItem;
 import hac.beans.ShoppingCart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +23,14 @@ public class CartController {
         return shoppingCart.getShoppingCart();
     }
 
+
     @PostMapping("/cart")
     public ResponseEntity<CartItem> addNewItem(@RequestBody final CartItem cartItem) {
         //add to session
         //TODO: check if exist- try this:
-        // if(!shoppingCart.getShoppingCart().containsKey(cartItem.getId())) //return error
+         if(shoppingCart.getShoppingCart().containsKey(cartItem.getId())){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+         }
 
         shoppingCart.add(cartItem);
         System.out.println("added "+ cartItem.getTitle());
