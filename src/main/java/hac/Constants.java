960efe0fc041c4001095cafb6e9803a8c9f1c335
Tali Kalev <talkalev@gmail.com>
2023-05-29 17:46:17 +0300

    added consts server side

---
+++ src/main/java/hac/Constants.java
@@ -0,0 +1,13 @@
+package hac;
+
+public final class Constants {
+    private Constants(){}
+
+    public static final String FIRST_NAME_MANDATORY = "First name is mandatory";
+    public static final String LAST_NAME_MANDATORY = "Last name is mandatory";
+    public static final String EMAIL_MANDATORY = "Email is mandatory";
+    public static final String EMAIL_VALID = "Email should be valid";
+    public static final String PAYMENT_POSITIVE = "Payment must be positive or zero";
+
+
+}
